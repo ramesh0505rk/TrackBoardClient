b7a731b9d911a187aa35818c27d01e62db15960e
Rameshkumar Krishnasamy <ramesh0505rk@gmail.com>
2026-09-24 09:51:47 +0530

    Updated sign-up component.

--- src/app/sign-up/sign-up.ts
+++ src/app/sign-up/sign-up.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Auth } from '../services/auth';
+import { UserService } from '../services/user-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.scss',
 })
 export class SignUp implements OnInit {
   signUpForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router, private authService: Auth) { }
+  constructor(private fb: FormBuilder, private router: Router, private authService: Auth, private userService: UserService) { }
 
   ngOnInit() {
     this.initializeSignUpForm();
@@ -20,7 +22,7 @@ export class SignUp implements OnInit {
 
   initializeSignUpForm() {
     this.signUpForm = this.fb.group({
-      userName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]],
+      userName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(25), Validators.pattern('^[a-zA-Z][a-zA-Z0-9_.-]*$')]],
       firstName: ['', [Validators.required]],
       lastName: [''],
       email: ['', [Validators.required, Validators.email]],
@@ -33,6 +35,18 @@ export class SignUp implements OnInit {
       this.signUpForm.markAllAsTouched();
       return;
     }
+
+    const { userName, firstName, lastName, email, password } = this.signUpForm.value;
+    this.userService.getTokenBySignUp(userName, firstName, lastName, email, password).subscribe({
+      next: (res: any) => {
+        localStorage.setItem('accessToken', res.accessToken);
+        this.authService.checkAuthStatus();
+        this.router.navigate(['/home']);
+      },
+      error: (err: any) => {
+
+      }
+    })
   }
 
   onSignInClick() {
